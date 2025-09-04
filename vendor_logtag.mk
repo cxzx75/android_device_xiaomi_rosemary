8cb7f74af6d3ca477b286fe535dad939fb5d16b7
cxzx75 <153367621+cxzx75@users.noreply.github.com>
2025-09-04 15:47:22 +0200

    rosemary: Silence GPUAUX logspam
    The logspam can make the device laggy especially when web-browsing

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -281,3 +281,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.RpAudioControl=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL) \
